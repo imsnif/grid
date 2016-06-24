d2cefd53d9b6126fecba5779fe5ad344c700c452
Aram Drevekenin <grimsniffer@gmail.com>
2016-06-25 00:45:51 +0300

    move window test cases

---
+++ test/cases/move.spec.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import test from 'tape'
+
+test.skip('able to move window to all parts of main display', async t => {
+  // TBD
+})
+
+test.skip('window cannot move up over another window', async t => {
+  // TBD
+})
+
+test.skip('window cannot move down over another window', async t => {
+  // TBD
+})
+
+test.skip('window cannot move left over another window', async t => {
+  // TBD
+})
+
+test.skip('window cannot move right over another window', async t => {
+  // TBD
+})
