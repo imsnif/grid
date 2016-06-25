71ec0cc5f86b27054436e0508b0951c259a6e312
Aram Drevekenin <grimsniffer@gmail.com>
2016-06-25 17:47:54 +0300

    window open test - passing

---
+++ main.js
@@ -0,0 +1,126 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+const globalShortcut = electron.globalShortcut
+const path = require('path')
+const window = require('electron-window')
+
+window.getFocusedWindow = window.getFocusedWindow || electron.BrowserWindow.getFocusedWindow
+
+app.on('ready', () => {
+  const screen = electron.screen
+  globalShortcut.register('CommandOrControl+l', () => {
+    const displays = screen.getAllDisplays()
+    const screenBounds = displays[0].bounds
+    const pane = window.getFocusedWindow()
+    console.log('pane:', pane)
+    if (pane) moveRight(screenBounds, pane)
+  })
+  globalShortcut.register('CommandOrControl+h', () => {
+    const displays = electron.screen.getAllDisplays()
+    const screenBounds = displays[0].bounds
+    const pane = window.getFocusedWindow()
+    if (pane) moveLeft(screenBounds, pane)
+  })
+  globalShortcut.register('CommandOrControl+j', () => {
+    const displays = electron.screen.getAllDisplays()
+    const screenBounds = displays[0].bounds
+    const pane = window.getFocusedWindow()
+    if (pane) moveDown(screenBounds, pane)
+  })
+  globalShortcut.register('CommandOrControl+k', () => {
+    const displays = electron.screen.getAllDisplays()
+    const screenBounds = displays[0].bounds
+    const pane = window.getFocusedWindow()
+    if (pane) moveUp(screenBounds, pane)
+  })
+  createWindow()
+  globalShortcut.register('CommandOrControl+w', createWindow)
+  globalShortcut.register('CommandOrControl+q', switchWindow)
+})
+
+function switchWindow () {
+  const pane = window.getFocusedWindow()
+  const allPanes = window.windows
+  const newWindowId = Object.keys(allPanes).filter(id => +id !== +pane.id)[0]
+  window.windows[newWindowId].focus()
+}
+
+function moveRight (workArea, pane) {
+  const bounds = pane.getBounds()
+  const x = bounds.x + 50 > workArea.width - bounds.width ? workArea.width - bounds.width : bounds.x + 50
+  const newBounds = Object.keys(window.windows)
+    .filter(id => id !== pane.id)
+    .some(id => {
+      const windowBounds = window.windows[id].getBounds()
+      return windowBounds.x - windowBounds.width === bounds.x &&
+        bounds.y > windowBounds.y - windowBounds.height &&
+        bounds.y < windowBounds.y + windowBounds.height
+    })
+    ? bounds
+    : Object.assign(bounds, {x})
+  pane.setBounds(newBounds)
+  console.log('new Bounds:', pane.getBounds())
+}
+
+function moveLeft (workArea, pane) {
+  const bounds = pane.getBounds()
+  const x = bounds.x - 50 < 0 ? 0 : bounds.x - 50
+  const newBounds = Object.keys(window.windows)
+    .filter(id => id !== pane.id)
+    .some(id => {
+      const windowBounds = window.windows[id].getBounds()
+      return windowBounds.x + windowBounds.width === bounds.x &&
+        bounds.y > windowBounds.y - windowBounds.height &&
+        bounds.y < windowBounds.y + windowBounds.height
+    })
+    ? bounds
+    : Object.assign(bounds, {x})
+  pane.setBounds(newBounds)
+  console.log('new Bounds:', pane.getBounds())
+}
+
+function moveUp (workArea, pane) {
+  const bounds = pane.getBounds()
+  const y = bounds.y - 50 < 0 ? 0 : bounds.y - 50
+  const newBounds = Object.keys(window.windows)
+    .filter(id => +id !== +pane.id)
+    .some(id => {
+      const windowBounds = window.windows[id].getBounds()
+      return windowBounds.y + windowBounds.height >= bounds.y &&
+        bounds.x > windowBounds.x - windowBounds.width &&
+        bounds.x < windowBounds.x + windowBounds.width
+    })
+    ? bounds
+    : Object.assign(bounds, {y})
+  pane.setBounds(newBounds)
+  console.log('new Bounds:', pane.getBounds())
+}
+
+function moveDown (workArea, pane) {
+  const bounds = pane.getBounds()
+  const y = bounds.y + 50 > workArea.height - bounds.height ? workArea.height - bounds.height : bounds.y + 50
+  const newBounds = Object.keys(window.windows)
+    .filter(id => id !== pane.id)
+    .some(id => {
+      const windowBounds = window.windows[id].getBounds()
+      return windowBounds.y - windowBounds.height === bounds.y &&
+        bounds.x > windowBounds.x - windowBounds.width &&
+        bounds.x < windowBounds.x + windowBounds.width
+    })
+    ? bounds
+    : Object.assign(bounds, {y})
+  pane.setBounds(newBounds)
+  console.log('new Bounds:', pane.getBounds())
+}
+
+function createWindow () {
+  console.log('creating window')
+  const created = window.createWindow({ width: 300, height: 200, frame: false })
+  const indexPath = path.resolve(__dirname, 'index.html')
+
+  created.showUrl(indexPath, () => {
+    console.log('window is now visible!')
+  })
+}
--- test/cases/move.spec.js
+++ test/cases/move.spec.js
@@ -1,6 +1,33 @@
 'use strict'
 
 import test from 'tape'
+import { Application } from 'spectron'
+import robot from 'robotjs'
+
+function createApp () {
+  return new Application({
+    path: './node_modules/.bin/electron',
+    args: ['./main.js']
+  })
+}
+
+test('able to open new window', async t => {
+  t.plan(1)
+  try {
+    const app = createApp()
+    await app.start()
+    robot.keyTap('w', 'control')
+    await app.browserWindow.blur()
+    await app.webContents.sendInputEvent({
+      type: 'char',
+      keyCode: 'w'
+    })
+    const windowCount = await app.client.getWindowCount()
+    t.equals(windowCount, 2, 'New window was opened')
+  } catch (e) {
+    t.fail(e)
+  }
+})
 
 test.skip('able to move window to all parts of main display', async t => {
   // TBD
