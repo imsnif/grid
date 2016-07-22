4eb81e4b7af1c26ff31905927c5eeaca5c3f5341
Aram Drevekenin <grimsniffer@gmail.com>
2016-07-22 15:28:22 +0300

    grid creation tests

---
+++ test/cases/grid-creation.spec.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import test from 'tape'
+import Grid from '../../'
+
+test('grid info', async t => {
+  t.plan(2)
+  const width = 1600
+  const height = 900
+  try {
+    const grid = new Grid({width, height})
+    t.deepEquals(grid.size, {width, height}, 'grid created with correct initial size')
+    t.equals(grid.windows.length, 0, 'grid created with no windows')
+  } catch (e) {
+    t.fail(e)
+    t.end()
+  }
+})
