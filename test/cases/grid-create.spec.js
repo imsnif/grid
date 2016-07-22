e0e11f2e2b971dd767071aa8551dede1d953e56f
Aram Drevekenin <grimsniffer@gmail.com>
2016-07-22 16:20:08 +0300

    grid create tests

--- test/cases/grid-create.spec.js
+++ test/cases/grid-create.spec.js
@@ -3,20 +3,24 @@
 import test from 'tape'
 import Grid from '../../'
 
+const WIDTH = 1600
+const HEIGHT = 900
+
 test('can create grid', async t => {
   t.plan(2)
-  const width = 1600
-  const height = 900
   try {
-    const grid = new Grid({width, height})
-    t.deepEquals(grid.size, {width, height}, 'grid created with correct initial size')
+    const grid = new Grid(WIDTH, HEIGHT)
+    t.deepEquals(grid.size, {width: WIDTH, height: HEIGHT}, 'correct initial size')
     t.equals(grid.windows.length, 0, 'grid created with no windows')
   } catch (e) {
-    t.fail(e)
+    t.fail(e.toString())
     t.end()
   }
 })
 
-test.skip('cannot create grid with bad parameters', async t => {
-  // TBD
+test('cannot create grid with bad parameters', async t => {
+  t.plan(3)
+  t.throws(new Grid(), new Error('provide width and height'))
+  t.throws(new Grid(undefined, HEIGHT), new Error('provide width and height'))
+  t.throws(new Grid('foo', HEIGHT), new Error('width and height should be numeric'))
 })
