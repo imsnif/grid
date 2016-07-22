b1047148868f3e0b949f55361c38109584a0665e
Aram Drevekenin <grimsniffer@gmail.com>
2016-07-22 21:19:48 +0300

    grid create tests passing

--- test/cases/grid-create.spec.js
+++ test/cases/grid-create.spec.js
@@ -6,7 +6,7 @@ import Grid from '../../'
 const WIDTH = 1600
 const HEIGHT = 900
 
-test('can create grid', async t => {
+test('can create grid', t => {
   t.plan(2)
   try {
     const grid = new Grid(WIDTH, HEIGHT)
@@ -18,9 +18,15 @@ test('can create grid', async t => {
   }
 })
 
-test('cannot create grid with bad parameters', async t => {
-  t.plan(3)
-  t.throws(new Grid(), new Error('provide width and height'))
-  t.throws(new Grid(undefined, HEIGHT), new Error('provide width and height'))
-  t.throws(new Grid('foo', HEIGHT), new Error('width and height should be numeric'))
+test('cannot create grid with bad parameters', t => {
+  t.plan(5)
+  try {
+    t.throws(() => new Grid(), Error, 'no width or height')
+    t.throws(() => new Grid(undefined, HEIGHT), Error, 'no width')
+    t.throws(() => new Grid(WIDTH), Error, 'no height')
+    t.throws(() => new Grid('foo', HEIGHT), Error, 'non numeric width')
+    t.throws(() => new Grid(WIDTH, 'bar'), Error, 'non numeric height')
+  } catch (e) {
+    t.fail(e.toString())
+  }
 })
