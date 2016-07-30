1467c7382eba4dfaae955bf70bdd1954cbfc761b
Aram Drevekenin <grimsniffer@gmail.com>
2016-07-30 19:09:33 +0300

    test structure

---
+++ test/cases/grid-add-window-generic.spec.js
@@ -0,0 +1,102 @@
+'use strict'
+
+import test from 'tape'
+import Grid from '../../'
+import _ from 'lodash'
+
+const WIDTH = 1600
+const HEIGHT = 900
+
+function StubWindow (id, width, height) {
+  this.id = id
+  this.width = width,
+  this.height = height
+}
+
+test('can add windows to grid', t => {
+  t.plan(4)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 600)
+    const stubWindow2 = new StubWindow(2, 400, 600)
+    let gridWindow
+    grid.add(stubWindow1)
+    t.equals(grid.windows.length, 1, 'grid has one window')
+    gridWindow = _.pick(grid.getWindow(1), ['id', 'width', 'height', 'x', 'y'])
+    t.deepEquals(gridWindow, Object.assign({}, stubWindow1, {
+      x: 0,
+      y: 0
+    }), 'window added in default location')
+    grid.add(stubWindow2, 950, 0)
+    t.equals(grid.windows.length, 2, 'grid has two windows')
+    gridWindow = _.pick(grid.getWindow(2), ['id', 'width', 'height', 'x', 'y'])
+    t.deepEquals(gridWindow, Object.assign({}, stubWindow2, {
+      x: 950,
+      y: 0
+    }), 'window added in custom location')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('windows should not be created over each other', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 200, 100)
+    const stubWindow2 = new StubWindow(2, 200, 100)
+    grid.add(stubWindow1)
+    t.throws(
+      () => grid.add(stubWindow2, 199, 0),
+      Error,
+      'window cannot be created over existing window on x axis'
+    )
+    t.throws(
+      () => grid.add(stubWindow2, 0, 99),
+      Error,
+      'window cannot be created over existing window on y axis'
+    )
+    t.throws(
+      () => grid.add(stubWindow2, 199, 99),
+      Error,
+      'window cannot be created over existing window on multiple axes'
+    )
+  } catch (e) {
+    t.fail(e)
+    t.end()
+  }
+})
+
+test('windows should not be created outside grid', t => {
+  t.plan(4)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow = new StubWindow(1, 200, 100)
+    const fatStubWindow = new StubWindow(2, WIDTH + 1, HEIGHT)
+    const tallStubWindow = new StubWindow(2, WIDTH, HEIGHT + 1)
+    t.throws(
+      () => grid.add(stubWindow, 1401, 0),
+      Error,
+      'window cannot exceed grid horizontal bounds'
+    )
+    t.throws(
+      () => grid.add(stubWindow, 0, 801),
+      Error,
+      'window canonot exceed grid vertical bounds'
+    )
+    t.throws(
+      () => grid.add(fatStubWindow),
+      Error,
+      'cannot add window wider than grid'
+    )
+    t.throws(
+      () => grid.add(tallStubWindow),
+      Error,
+      'cannot add window taller than grid'
+    )
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
---
+++ test/cases/grid-change-window-generic.spec.js
@@ -0,0 +1,203 @@
+'use strict'
+
+import test from 'tape'
+import Grid from '../../'
+import _ from 'lodash'
+
+const WIDTH = 1600
+const HEIGHT = 900
+
+function StubWindow (id, width, height) {
+  this.id = id
+  this.width = width,
+  this.height = height
+}
+
+test('can increase window size', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow = new StubWindow(1, 400, 600)
+    grid.add(stubWindow)
+    grid.getWindow(1).changeSize(450, 650)
+    t.equals(grid.windows.length, 1, 'grid has one window')
+    t.deepEquals(_.pick(grid.getWindow(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 450,
+      height: 650
+    }, 'window size changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('can decrease window size', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow = new StubWindow(1, 400, 600)
+    grid.add(stubWindow)
+    grid.getWindow(1).changeSize(350, 550)
+    t.equals(grid.windows.length, 1, 'grid has one window')
+    t.deepEquals(_.pick(grid.getWindow(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 350,
+      height: 550
+    }, 'window size changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('cannot resize window over another window', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 100)
+    const stubWindow2 = new StubWindow(2, 400, 100)
+    const stubWindow3 = new StubWindow(3, 400, 100)
+    grid.add(stubWindow1)
+    grid.add(stubWindow2, 400)
+    grid.add(stubWindow3, 0, 100)
+    t.throws(
+      () => grid.getWindow(1).changeSize(401, 100),
+      Error,
+      'cannot resize window horizontally over another'
+    )
+    t.throws(
+      () => grid.getWindow(1).changeSize(400, 101),
+      Error,
+      'cannot resize window vertically over another'
+    )
+    t.equals(grid.windows.length, 3, 'grid windows still present')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('can change window location', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow = new StubWindow(1, 400, 600)
+    grid.add(stubWindow)
+    grid.getWindow(1).changeLocation(1, 1)
+    t.equals(grid.windows.length, 1, 'grid has one window')
+    t.deepEquals(_.pick(grid.getWindow(1), ['x', 'y', 'width', 'height']), {
+      x: 1,
+      y: 1,
+      width: 400,
+      height: 600
+    }, 'window location changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('cannot move window over another window', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 100)
+    const stubWindow2 = new StubWindow(2, 400, 100)
+    const stubWindow3 = new StubWindow(3, 400, 100)
+    grid.add(stubWindow1)
+    grid.add(stubWindow2, 400)
+    grid.add(stubWindow3, 0, 100)
+    t.throws(
+      () => grid.getWindow(1).changeLocation(1, 0),
+      Error,
+      'cannot move window horizontally over another'
+    )
+    t.throws(
+      () => grid.getWindow(1).changeLocation(0, 1),
+      Error,
+      'cannot move window vertically over another'
+    )
+    t.equals(grid.windows.length, 3, 'grid windows still present')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('cannot resize window outside grid', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 100)
+    grid.add(stubWindow1)
+    t.throws(
+      () => grid.getWindow(1).changeSize(1601, 100),
+      Error,
+      'cannot resize window horizontally outside grid'
+    )
+    t.throws(
+      () => grid.getWindow(1).changeSize(400, 901),
+      Error,
+      'cannot resize window vertically outside grid'
+    )
+    t.equals(grid.windows.length, 1, 'grid window still present')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('cannot move window outside grid', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 100)
+    grid.add(stubWindow1)
+    t.throws(
+      () => grid.getWindow(1).changeLocation(1201, 100),
+      Error,
+      'cannot move window horizontally outside grid'
+    )
+    t.throws(
+      () => grid.getWindow(1).changeLocation(400, 801),
+      Error,
+      'cannot move window vertically outside grid'
+    )
+    t.equals(grid.windows.length, 1, 'grid window still present')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('can move window into location vacated by another', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    const stubWindow1 = new StubWindow(1, 400, 100)
+    const stubWindow2 = new StubWindow(2, 400, 100)
+    grid.add(stubWindow1)
+    grid.add(stubWindow2, 400)
+    grid.getWindow(1).changeLocation(0, 100)
+    grid.getWindow(2).changeLocation(0, 0)
+    t.equals(grid.windows.length, 2, 'grid windows still present')
+    t.deepEquals(_.pick(grid.getWindow(2), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 100
+    }, 'window size changed')
+    t.deepEquals(_.pick(grid.getWindow(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 100,
+      width: 400,
+      height: 100
+    }, 'window size changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
