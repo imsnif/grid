e2d218d45658b40b9c1f31ae1107d913316d52ae
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-05 12:55:37 +0300

    occupy-window unit test cases

--- test/unit/occupy-window.spec.js
+++ test/unit/occupy-window.spec.js
@@ -1,5 +1,29 @@
 import test from 'tape'
 
-test.skip('write test cases', t => {
+test.skip('occupy(representation, window): occupy window on representation', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, window): bad parameters', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, window, windowPrev): occupy window on representation', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, windowm windowPrev): bad parameters', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, windowm windowPrev): size exceeds grid', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, windowm windowPrev): space is occupied', t => {
+  // TBD
+})
+
+test.skip('occupy(representation, windowm windowPrev): representation is corrupt', t => {
   // TBD
 })
