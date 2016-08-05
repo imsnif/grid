c37662096e3b614605566f6c9f15512ad9fadd97
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-05 13:26:46 +0300

    occupy window unit tests

--- test/unit/occupy-window.spec.js
+++ test/unit/occupy-window.spec.js
@@ -1,7 +1,42 @@
 import test from 'tape'
+import occupy from '../../lib/occupy-window'
 
-test.skip('occupy(representation, window): occupy window on representation', t => {
-  // TBD
+function createRepresentation (width, height) {
+  return new Array(height)
+    .fill([]) // create two dimensional array
+    .map(row => new Array(width).fill(0)) // zero fill all cells
+}
+
+function createWindow (id, x, y, width, height) {
+  return {id, x, y, width, height}
+}
+
+function isOccupied (representation, window) {
+  const firstVerticalPoint = window.y
+  const firstHorizontalPoint = window.x
+  const lastVerticalPoint = window.height + window.y
+  const lastHorizontalPoint = window.width + window.x
+  for (let y = firstVerticalPoint; y < lastVerticalPoint; y += 1) {
+    for (let x = firstHorizontalPoint; x < lastHorizontalPoint; x += 1) {
+      if (representation[y][x] !== window.id) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+test('occupy(representation, window): occupy window on representation', t => {
+  t.plan(1)
+  try {
+    const initialRepresentation = createRepresentation(1600, 900)
+    const window = createWindow(1, 0, 0, 100, 100)
+    const newRepresentation = occupy(initialRepresentation, window)
+    t.ok(isOccupied(newRepresentation, window), 'window occupied on grid representation')
+  } catch (err) {
+    t.fail(err.toString())
+    t.end()
+  }
 })
 
 test.skip('occupy(representation, window): bad parameters', t => {
