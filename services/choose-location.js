72eed651f81033aff79a0c4d62ec7a337a6b75af
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-21 22:50:13 +0300

    refactor: optimized choose-location algorithm

--- services/choose-location.js
+++ services/choose-location.js
@@ -7,6 +7,7 @@ const occupy = require('./occupy-pane')
 
 function occupySpace (seq, pane, representation, y) {
   try {
+    checkDiagonally(representation, Object.assign({}, pane, {x: seq.start, y}))
     occupy(representation, Object.assign({}, pane, {x: seq.start, y}))
     return ({x: seq.start, y})
   } catch (e) {
@@ -15,6 +16,9 @@ function occupySpace (seq, pane, representation, y) {
       typeof e.coords.x === 'undefined' ||
       e.coords.x > seq.end - pane.width + 1
     ) {
+      if (typeof e.coords !== 'undefined') {
+        throw e
+      }
       return false
     }
     if (e.coords.x === seq.start) {
@@ -24,19 +28,59 @@ function occupySpace (seq, pane, representation, y) {
   }
 }
 
+function checkDiagonally (representation, pane) {
+  if (pane.width >= pane.height) {
+    let x = pane.x
+    for (let y = pane.y; y <= pane.height; y += 1) {
+      if (
+        !Array.isArray(representation[y]) ||
+        typeof representation[y][x] === 'undefined' ||
+        representation[y][x] !== 0
+      ) {
+        const err = new Error('blocked')
+        err.coords = {x, y}
+        throw err
+      }
+      x = x === pane.x + pane.width ? pane.x : x + 1
+    }
+  } else { // TODO: fix this
+    let y = pane.y
+    for (let x = pane.x; x <= pane.width; x += 1) {
+      if (
+        !Array.isArray(representation[y]) ||
+        typeof representation[y][x] === 'undefined' ||
+        representation[y][x] !== 0
+      ) {
+        const err = new Error('blocked')
+        err.coords = {x, y}
+        throw err
+      }
+      y = y === pane.y + pane.height ? pane.y : y + 1
+    }
+  }
+}
+
 module.exports = function chooseLocation (prevRepresentation, pane) {
   assert(validate.isObject(prevRepresentation))
   assert(validate.isObject(pane))
   let representation = prevRepresentation.map(r => r.slice())
-  const gridHeight = representation.length
+  const gridHeight = representation.length - 1
   let occupied
-  for (let y = 0; y < gridHeight - pane.height; y += 1) {
+  let y = 0
+  while (y <= gridHeight - pane.height + 1) {
     const emptySequences = findSequence(representation[y], 0)
+    let blockedYs = []
     emptySequences.filter(s => s.end - s.start + 1 >= pane.width).some(s => {
-      occupied = occupySpace(s, pane, representation, y)
-      return occupied
+      try {
+        occupied = occupySpace(s, pane, representation, y)
+        return occupied
+      } catch (e) {
+        blockedYs.push(e.coords.y)
+        return false
+      }
     })
     if (occupied) return occupied
+    y = blockedYs.length > 0 ? Math.min(blockedYs) : y + 1
   }
   throw new Error('no space for new pane')
 }
