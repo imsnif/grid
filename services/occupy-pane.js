322a63b2040f468df01911c53278d1253d4de882
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-29 17:32:03 +0300

    refactor: occupy-pane

--- services/occupy-pane.js
+++ services/occupy-pane.js
@@ -3,38 +3,37 @@
 const assert = require('assert')
 const validate = require('validate.js')
 
-module.exports = function occupy (grid, candidate) {
-  assert(validate.isObject(grid))
-  assert(validate.isObject(candidate))
+function detectCollision (pane, candidate) {
   if (
-    candidate.x + candidate.width > grid.width ||
-    candidate.y + candidate.height > grid.height
+    candidate.x < pane.x + pane.width &&
+    candidate.x + candidate.width > pane.x &&
+    candidate.y < pane.y + pane.height &&
+    candidate.height + candidate.y > pane.y
   ) {
-    throw new Error('size exceeds grid')
+    return true
   }
+}
+
+function heightOffsetOrder (a, b) {
+  return (a.y + a.height <= b.y + b.height ? -1 : 1)
+}
+
+module.exports = function occupy (grid, candidate) {
+  assert(validate.isObject(grid), `${grid} is not an object`)
+  assert(validate.isObject(candidate), `${candidate} is not an object`)
+  assert(candidate.x + candidate.width <= grid.width, 'size exceeds grid')
+  assert(candidate.y + candidate.height <= grid.height, 'size exceeds grid')
   const colliders = grid.panes
     .filter(p => p.id !== candidate.id)
-    .filter((pane) => {
-      if (
-        candidate.x < pane.x + pane.width &&
-        candidate.x + candidate.width > pane.x &&
-        candidate.y < pane.y + pane.height &&
-        candidate.height + candidate.y > pane.y
-      ) {
-        return true
-      }
-    })
-    .sort((a, b) => {
-      if (a.y + a.height <= b.y + b.height) {
-        return -1
-      } else {
-        return 1
-      }
-    })
+    .filter((pane) => detectCollision(pane, candidate))
+    .sort(heightOffsetOrder)
   if (colliders.length > 0) {
     const err = new Error('space is occupied')
-    err.coords = {x: colliders[0].x + colliders[0].width, y: colliders[0].y + colliders[0].height}
+    err.coords = {
+      x: colliders[0].x + colliders[0].width,
+      y: colliders[0].y + colliders[0].height
+    }
     throw err
   }
-  return {x: candidate.x, y: candidate.y}
+  return candidate
 }
