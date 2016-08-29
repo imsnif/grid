f973dd1da8cd8505ac9f0b364e4c241a24bb3970
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-29 20:39:00 +0300

    refactor: choose-location

--- services/choose-location.js
+++ services/choose-location.js
@@ -4,30 +4,38 @@ const assert = require('assert')
 const validate = require('validate.js')
 const occupy = require('./occupy-pane')
 
-module.exports = function chooseLocation (grid, pane) {
-  assert(validate.isObject(grid))
-  assert(validate.isObject(pane))
-  let y = 0
-  while (y !== false) {
-    let x = 0
-    let blockedLines = []
-    while (x !== false) {
-      try {
-        const occupied = occupy(grid, Object.assign({}, pane, {x, y}))
-        return occupied
-      } catch (e) {
-        blockedLines.push(e.coords.y)
-        if (grid.width >= e.coords.x + pane.width) {
-          x = e.coords.x
-        } else {
-          x = false
-        }
-      }
-    }
-    if (grid.height >= Math.min(...blockedLines) + pane.height) {
-      y = Math.min(...blockedLines)
+function checkLines (opts) {
+  try {
+    return checkColumns(opts)
+  } catch (e) {
+    if (opts.grid.height >= Math.min(...e.blockedLines) + opts.pane.height) {
+      // if there's room left below highest block point, check that line
+      return checkLines(Object.assign({}, opts, {y: Math.min(...e.blockedLines)}))
     } else {
       throw new Error('no space for new pane')
     }
   }
+}
+
+function checkColumns (opts) {
+  opts.blockedLines = opts.blockedLines || []
+  try {
+    return occupy(opts.grid, Object.assign({}, opts.pane, {x: opts.x, y: opts.y}))
+  } catch (e) {
+    opts.blockedLines.push(e.coords.y)
+    if (opts.grid.width >= e.coords.x + opts.pane.width) {
+      // if there's room left to the right of the block, keep checking...
+      return checkColumns(Object.assign({}, opts, {x: e.coords.x}))
+    } else {
+      const err = new Error('end of the line')
+      err.blockedLines = opts.blockedLines
+      throw err
+    }
+  }
+}
+
+module.exports = function chooseLocation (grid, pane) {
+  assert(validate.isObject(grid))
+  assert(validate.isObject(pane))
+  return checkLines({grid, pane, x: 0, y: 0})
 }
