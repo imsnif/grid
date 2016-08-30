9ffa1ecc5c84ec6d7e49bb16e0aede4e82b52825
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-30 10:10:07 +0300

    test: max resize down with obstructing windows

--- test/unit/pane-wrapper.spec.js
+++ test/unit/pane-wrapper.spec.js
@@ -156,8 +156,24 @@ test('wrapper.maxSize(opts): can max pane size down', t => {
   }
 })
 
-test.skip('wrapper.maxSize(opts): can max pane size down with obstructing window', t => {
-  // TBD
+test('wrapper.maxSize(opts): can max pane size down with obstructing windows', t => {
+  t.plan(1)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubWindow, {id: 1, width: 400, height: 200})
+    grid.add(StubWindow, {id: 2, width: 400, height: 200, x: 0, y: 400})
+    grid.add(StubWindow, {id: 2, width: 400, height: 200, x: 0, y: 700})
+    grid.getPane(1).maxSize({down: true})
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 400
+    }, 'pane size changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
 })
 
 test('wrapper.maxSize(opts): can max pane size up', t => {
