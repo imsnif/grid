7ec662f2364dfb86926bbfdc19a1ade2508ff90c
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-30 10:26:16 +0300

    test: max resize down electron

--- test/integration/grid-change-window-electron.spec.js
+++ test/integration/grid-change-window-electron.spec.js
@@ -2,6 +2,7 @@
 
 import test from 'tape'
 import Grid from '../../'
+import _ from 'lodash'
 
 const WIDTH = 1600
 const HEIGHT = 900
@@ -227,6 +228,24 @@ test('grid can decide pane location horizontally ', t => {
       width: 400,
       height: 100
     }, 'pane location decided properly')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('wrapper.maxSize(opts): can max pane size down (electron)', t => {
+  t.plan(1)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(BrowserWindow, {id: 1, width: 400, height: 600})
+    grid.getPane(1).maxSize({down: true})
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 900
+    }, 'pane size changed')
   } catch (e) {
     t.fail(e.toString())
     t.end()
