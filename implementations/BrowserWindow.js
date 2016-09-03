890db1adc3b4e8c0397ae9385b1c120ed1f4ec74
Aram Drevekenin <grimsniffer@gmail.com>
2016-09-03 14:22:20 +0300

    feat: grid offset

--- implementations/BrowserWindow.js
+++ implementations/BrowserWindow.js
@@ -8,15 +8,20 @@ module.exports = {
     state.wrapped.setBounds(Object.assign({}, bounds, {width, height}))
   },
   changeLocation: (state, x, y) => {
+    const offset = state.grid.offset || { x: 0, y: 0 }
     const bounds = state.wrapped.getBounds()
-    state.wrapped.setBounds(Object.assign({}, bounds, {x, y}))
+    state.wrapped.setBounds(Object.assign({}, bounds, {
+      x: x + offset.x,
+      y: y + offset.y
+    }))
   },
   changeBounds: (state) => {
+    const offset = state.grid.offset || { x: 0, y: 0 }
     state.wrapped.setBounds({
       width: state.width,
       height: state.height,
-      x: state.x,
-      y: state.y
+      x: state.x + offset.x,
+      y: state.y + offset.y
     })
   }
 }
