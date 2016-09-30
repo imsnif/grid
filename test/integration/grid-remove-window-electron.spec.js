6181fc6bea69f6c84411ebd45f7e8c1b86fddb5e
Aram Drevekenin <grimsniffer@gmail.com>
2016-09-30 16:36:54 +0300

    test: remove pane from grid on wrappee close

--- test/integration/grid-remove-window-electron.spec.js
+++ test/integration/grid-remove-window-electron.spec.js
@@ -2,11 +2,13 @@
 import test from 'tape'
 import Grid from '../../'
 import sinon from 'sinon'
+import {EventEmitter} from 'events'
 const WIDTH = 1600
 const HEIGHT = 900
 
 function BrowserWindow (opts) {
   // mock electron BrowserWindow
+  this._emitter = new EventEmitter()
   this.id = opts.id
   this.width = opts.width
   this.height = opts.height
@@ -22,7 +24,15 @@ BrowserWindow.prototype.getBounds = function () {
 }
 
 BrowserWindow.prototype.close = function () {
-  // no-op
+  this.emit('close')
+}
+
+BrowserWindow.prototype.on = function () {
+  this._emitter.on.apply(this._emitter, arguments)
+}
+
+BrowserWindow.prototype.emit = function () {
+  this._emitter.emit.apply(this._emitter, arguments)
 }
 
 BrowserWindow.prototype.setBounds = function (bounds) {
@@ -41,6 +51,20 @@ test('can remove electron BrowserWindow', t => {
     grid.remove(1)
     t.equal(grid.panes.length, 0, 'grid removed from pane')
     t.ok(spy.calledOnce, 'close method of BrowserWindow was caled only once')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('BrowserWindow.close() removes pane from grid', t => {
+  t.plan(1)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(BrowserWindow, {id: 1, width: 400, height: 600})
+    const pane = grid.getPane(1)
+    pane.close()
+    t.equal(grid.panes.length, 0, 'grid removed from pane')
   } catch (e) {
     t.fail(e.toString())
     t.end()
