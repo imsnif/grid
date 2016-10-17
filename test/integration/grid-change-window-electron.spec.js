93959385983da5afc049711d1c2fee88c3582af9
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-17 17:09:30 +0300

    test: max pane location down electron

--- test/integration/grid-change-window-electron.spec.js
+++ test/integration/grid-change-window-electron.spec.js
@@ -294,6 +294,24 @@ test('wrapper.maxOrSkipLoc(opts): can max pane location down (electron)', t => {
   try {
     const grid = new Grid(WIDTH, HEIGHT)
     grid.add(BrowserWindow, {id: 1, width: 400, height: 600})
+    grid.getPane(1).maxLoc({down: true})
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 300,
+      width: 400,
+      height: 600
+    }, 'pane size changed')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('wrapper.maxOrSkipLoc(opts): can max pane location down with maxOrskipLoc (electron)', t => {
+  t.plan(1)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(BrowserWindow, {id: 1, width: 400, height: 600})
     grid.getPane(1).maxOrSkipLoc({down: true})
     t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
       x: 0,
