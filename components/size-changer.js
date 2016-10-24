ca000febbd407da3cd5cfa34137acda9e178ab1c
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-24 13:42:12 +0300

    feat: increase size directional to max if not enough room

--- components/size-changer.js
+++ components/size-changer.js
@@ -85,24 +85,24 @@ module.exports = function sizeChanger (state, implementation) {
         `${direction} must be one of right/left/up/down`
       )
       assert(validate.isInteger(amount), `${amount} must be numeric`)
-      assert(
-        ((direction === 'right') && state.x + state.width + amount <= state.grid.width) ||
-        ((direction === 'left') && state.x - amount >= 0) ||
-        ((direction === 'up') && state.y - amount >= 0) ||
-        ((direction === 'down') && state.y + state.height + amount <= state.grid.height),
-        'size exceeds grid'
-      )
       const x = direction === 'left' ? state.x - amount
         : state.x
       const y = direction === 'up' ? state.y - amount
         : state.y
       const width = direction === 'left' || direction === 'right' ? state.width + amount : state.width
       const height = direction === 'up' || direction === 'down' ? state.height + amount : state.height
-      occupy(
-        state.grid,
-        Object.assign({}, state, {x, y, width, height})
-      )
-      // TODO: max in direction if occupy fails
+      try {
+        occupy(
+          state.grid,
+          Object.assign({}, state, {x, y, width, height})
+        )
+      } catch (e) {
+        if (e.message === 'space is occupied' || e.message === 'size exceeds grid') {
+          return state.maxSize({[direction]: true})
+        } else {
+          throw e
+        }
+      }
       state.x = x
       state.y = y
       state.width = width
