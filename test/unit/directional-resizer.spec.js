c732fca10b7fd0a1b646aedd63b62566fdc4304b
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-26 00:00:38 +0300

    test: increase/decrease and fill size

---
+++ test/unit/directional-resizer.spec.js
@@ -0,0 +1,170 @@
+'use strict'
+
+import test from 'tape'
+import Grid from '../../lib/grid'
+import _ from 'lodash'
+
+const WIDTH = 1600
+const HEIGHT = 900
+
+function StubPane (opts) {
+  this.id = opts.id
+  this.width = opts.width
+  this.height = opts.height
+}
+
+test('increaseAndFillsize(direction, amount) - can increase size over other pane', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 800, height: 900})
+    grid.add(StubPane, {id: 2, x: 800, y: 0, width: 800, height: 900})
+    grid.getPane(1).increaseAndFillSize('right', 30)
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 830,
+      height: 900
+    }, 'pane size increased')
+    t.deepEquals(_.pick(grid.getPane(2), ['x', 'y', 'width', 'height']), {
+      x: 830,
+      y: 0,
+      width: 770,
+      height: 900
+    }, 'adjacent pane decreased in size')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('increaseAndFillsize(direction, amount) - bad params', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 1600, height: 900})
+    t.throws(
+      () => grid.getPane(1).increaseAndFillSize('notDirection', 30),
+      /notDirection must be one of right\/left\/up\/down/,
+      'cannot call increaseAndFillSize with bad direction'
+    )
+    t.throws(
+      () => grid.getPane(1).increaseAndFillSize('right', 'notANumber'),
+      /notANumber must be numeric/,
+      'cannot call increaseAndFillSize with bad amount'
+    )
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('increaseAndFillsize(direction, amount) - cascade size increase to multiple panes', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 800, height: 900})
+    grid.add(StubPane, {id: 2, x: 800, y: 0, width: 10, height: 900})
+    grid.add(StubPane, {id: 3, x: 810, y: 0, width: 790, height: 900})
+    grid.getPane(3).increaseAndFillSize('left', 30)
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 770,
+      height: 900
+    }, 'pane resize cascaded')
+    t.deepEquals(_.pick(grid.getPane(2), ['x', 'y', 'width', 'height']), {
+      x: 770,
+      y: 0,
+      width: 10,
+      height: 900
+    }, 'pane too small to resize pushed')
+    t.deepEquals(_.pick(grid.getPane(3), ['x', 'y', 'width', 'height']), {
+      x: 780,
+      y: 0,
+      width: 820,
+      height: 900
+    }, 'pane resized')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('increaseAndFillsize(direction, amount) - parallel pane also increases in size to fill in gap', t => {
+  t.plan(3)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 800, height: 900})
+    grid.add(StubPane, {id: 2, x: 800, y: 0, width: 800, height: 450})
+    grid.add(StubPane, {id: 3, x: 800, y: 450, width: 800, height: 450})
+    grid.getPane(2).increaseAndFillSize('left', 30)
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 770,
+      height: 900
+    }, 'adjacent pane resized')
+    t.deepEquals(_.pick(grid.getPane(2), ['x', 'y', 'width', 'height']), {
+      x: 770,
+      y: 0,
+      width: 830,
+      height: 450
+    }, 'pane resized')
+    t.deepEquals(_.pick(grid.getPane(3), ['x', 'y', 'width', 'height']), {
+      x: 770,
+      y: 450,
+      width: 830,
+      height: 450
+    }, 'parallel pane resized')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('decreaseAndFillSize(direction, amount) - can decrease size and have adjacent pane fill gaps', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 800, height: 900})
+    grid.add(StubPane, {id: 2, x: 800, y: 0, width: 800, height: 900})
+    grid.getPane(1).decreaseAndFillSize('left', 30)
+    t.deepEquals(_.pick(grid.getPane(1), ['x', 'y', 'width', 'height']), {
+      x: 0,
+      y: 0,
+      width: 770,
+      height: 900
+    }, 'pane size decreased')
+    t.deepEquals(_.pick(grid.getPane(2), ['x', 'y', 'width', 'height']), {
+      x: 770,
+      y: 0,
+      width: 830,
+      height: 900
+    }, 'adjacent pane increased in size')
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
+
+test('decreaseAndFillSize(direction, amount) - bad params', t => {
+  t.plan(2)
+  try {
+    const grid = new Grid(WIDTH, HEIGHT)
+    grid.add(StubPane, {id: 1, x: 0, y: 0, width: 1600, height: 900})
+    t.throws(
+      () => grid.getPane(1).decreaseAndFillSize('notDirection', 30),
+      /notDirection must be one of right\/left\/up\/down/,
+      'cannot call increaseAndFillSize with bad direction'
+    )
+    t.throws(
+      () => grid.getPane(1).decreaseAndFillSize('right', 'notANumber'),
+      /notANumber must be numeric/,
+      'cannot call increaseAndFillSize with bad amount'
+    )
+  } catch (e) {
+    t.fail(e.toString())
+    t.end()
+  }
+})
