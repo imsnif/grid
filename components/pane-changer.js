7b2a03c3d6bd4af73cfbe5b9ad6641b34efe01eb
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-29 22:23:51 +0300

    feat: switch panes

--- components/pane-changer.js
+++ components/pane-changer.js
@@ -13,6 +13,24 @@ module.exports = function paneChanger (state) {
     },
     findGaps: function findGaps () {
       return findNextGaps(state, {x: 0, y: 0})
+    },
+    switchPanes: function switchPanes (firstId, secondId) {
+      const firstPane = state.getPane(firstId)
+      const secondPane = state.getPane(secondId)
+      const firstLocation = {
+        x: firstPane.x,
+        y: firstPane.y,
+        height: firstPane.height,
+        width: firstPane.width
+      }
+      const secondLocation = {
+        x: secondPane.x,
+        y: secondPane.y,
+        height: secondPane.height,
+        width: secondPane.width
+      }
+      firstPane.overrideLocation(secondLocation)
+      secondPane.overrideLocation(firstLocation)
     }
   })
 }
